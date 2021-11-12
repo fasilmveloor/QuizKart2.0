9bf456cce7984fd32e4e936f89ed936963f7d5e1
Fasil Veloor <fasilmveloor@gmail.com>
2021-11-12 18:58:11 +0530

    Onboarding Screen with initial design

--- app/src/main/java/com/example/quizkart/MainActivity.java
+++ app/src/main/java/com/example/quizkart/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.quizkart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 import com.example.quizkart.databinding.ActivityMainBinding;
@@ -17,5 +19,12 @@ public class MainActivity extends AppCompatActivity {
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = activityMainBinding.getRoot();
         setContentView(view);
+        int SPLASH_SCREEN_TIME_OUT = 5000;
+        new Handler().postDelayed(() -> {
+            Intent i=new Intent(MainActivity.this, OnBoardingActivity.class);
+
+            startActivity(i); //Invoke second Activity
+            finish();//the current activity will get finished.
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
---
+++ app/src/main/java/com/example/quizkart/OnBoardingActivity.java
@@ -0,0 +1,54 @@
+package com.example.quizkart;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.quizkart.OnBoarding.OnBoardingAdapter;
+import com.example.quizkart.OnBoarding.OnBoardingPageTransformer;
+import com.example.quizkart.databinding.ActivityOnBoardingBinding;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    ActivityOnBoardingBinding activityOnBoardingBinding;
+    OnBoardingAdapter onBoardingAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityOnBoardingBinding = ActivityOnBoardingBinding.inflate(getLayoutInflater());
+        View view = activityOnBoardingBinding.getRoot();
+        setContentView(view);
+
+        makeStatusBarTransparent();
+        onBoardingAdapter = new OnBoardingAdapter(this);
+        activityOnBoardingBinding.onBoardingViewPager.setAdapter(onBoardingAdapter);
+        activityOnBoardingBinding.onBoardingViewPager.setPageTransformer(false, new OnBoardingPageTransformer());
+
+    }
+
+    public void nextPage(View view) {
+        if (view.getId() == R.id.button2) {
+            if (activityOnBoardingBinding.onBoardingViewPager.getCurrentItem() < onBoardingAdapter.getCount() - 1) {
+                activityOnBoardingBinding.onBoardingViewPager.setCurrentItem(activityOnBoardingBinding.onBoardingViewPager.getCurrentItem() + 1, true);
+            }
+            /*else {
+                Intent i = new Intent(OnBoardingActivity.this, LoginTestActivity.class);
+                startActivity(i);
+            }*/
+        }
+    }
+
+    private void makeStatusBarTransparent() {
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.TRANSPARENT);
+    }
+}
